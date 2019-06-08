06dc0d6c0abba699f1b022a81bfe7b8f41d1ef81
Francisco Presencia <public@francisco.io>
2019-06-08 22:05:25 +0900

    Upgraded

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from "react";
 
+const defFallback = () => null;
+const defHandler = ({ error }) => error;
+
 export default (ld, cb, err) => ({
-  loader = () => null,
+  fallback,
+  handler,
   dependencies = [],
   ...props
 }) => {
-  if (cb) {
-    loader = loader || ld;
-    ld = cb;
-  }
+  // Take them from the right arguments
+  const [Loader, callback, Handler] = cb
+    ? [fallback || ld || defFallback, cb, handler || err || defHandler]
+    : [fallback || defFallback, ld, handler || err || defHandler];
 
-  const Loader = loader;
-  const ErrorHandler = err || (({ error }) => error);
+  const ErrorHandler = err || handler;
   const [data, setData] = useState(<Loader {...props} />);
   useEffect(() => {
-    Promise.resolve(ld(props))
+    Promise.resolve(callback(props))
       .then(data => setData(data || null))
       .catch(error => setData(<ErrorHandler error={error.message} />));
   }, dependencies);
